78659e5fb2f449da1028b29f21c59944053ae029
Frieve-A <frievea@gmail.com>
2025-03-18 09:46:49 +0900

    Fix

---
+++ scripts/prepare-icons.js
@@ -0,0 +1,17 @@
+const fs = require('fs-extra');
+const path = require('path');
+const png2icons = require('png2icons');
+
+(async () => {
+  const source = path.join(__dirname, '../images/icon.png');
+  const destDir = path.join(__dirname, '../build/favicon.icns');
+  await fs.ensureDir(destDir);
+  const input = await fs.readFile(source);
+  const output = png2icons.createICNS(input, png2icons.BICUBIC, 0);
+  if (output) {
+    await fs.writeFile(destDir, output);
+    console.log('favicon.icns created.');
+  } else {
+    throw new Error('ICNS creation failed.');
+  }
+})();
